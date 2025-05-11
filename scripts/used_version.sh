0cca53c25de3c46202b80ddc1cf93e6b79bd5c4f
DarkSorrow <darksorrow.slade@gmail.com>
2025-05-11 16:15:37 +0200

    chore: update llama.cpp version

--- scripts/used_version.sh
+++ scripts/used_version.sh
@@ -4,11 +4,11 @@
 
 # The specific llama.cpp commit hash we want to use
 # Using a specific commit hash ensures a consistent build
-LLAMA_CPP_COMMIT="7c28a74e0783f4bb74a246fb9f19bf212139e365"  # Commit as specified by user
+LLAMA_CPP_COMMIT="7f323a589f8684c0eb722e7309074cb5eac0c8b5"  # Commit as specified by user
 
 # The tag to use for prebuilt binaries
 # This might differ from the commit hash format
-LLAMA_CPP_TAG="b5332"  # Tag format for binary downloads
+LLAMA_CPP_TAG="b5346"  # Tag format for binary downloads
 
 # Vulkan and OpenCL versions
 VULKAN_SDK_VERSION="1.4.309"
