a770fe5237e4bad0695df81de8f00ad2323e3ad6
DarkSorrow <darksorrow.slade@gmail.com>
2025-05-11 16:51:16 +0200

    test: android failing

--- cpp/build-info.cpp
+++ cpp/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 5332;
-char const *LLAMA_COMMIT = "7c28a74e";
+int LLAMA_BUILD_NUMBER = 5346;
+char const *LLAMA_COMMIT = "7f323a58";
 char const *LLAMA_COMPILER = "unknown";
 char const *LLAMA_BUILD_TARGET = "unknown";
